fc6d9413652e896533961eb481594b1b868fef81
darshilpanchal22 <panchaldarshil145@gmail.com>
2026-02-24 11:36:52 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
+import Project from "./components/Project";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Project />
+      <Reviews />
+      <Contact />
     </div>
   );
 };
